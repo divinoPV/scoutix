2806c65a54e5ccad9d2ae97c68591fa8ffab43ec
divino <monteiro.hugo2001@icloud.com>
2022-05-02 17:23:33 +0200

    feature(#42)[separate BO & FO with condition in index.tsx]

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,17 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from './app/App';
+import Admin from './admin/Admin';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {window.location.origin.includes('iplmi03kw9aw.') ? <Admin /> : <App />}
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('CAKVPC5r8tJ9')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
