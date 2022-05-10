82665724315d1cf6d29d564d48de637a7c7dbd85
spacelocust <lucasboganin@hotmail.fr>
2022-05-10 02:37:36 +0200

    Feature(#15)[Refactor]: make crud event category

---
+++ api/src/Controller/Global/EventCategoryController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller\Global;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\EventCategorytory;
+
+class EventCategoryController extends AbstractController
+{
+    public function __invoke(EventCategorytory $repoEventCateg)
+    {
+        $eventCategories = $repoEventCateg->findAvailable();
+        return $eventCategories;
+    }
+}
